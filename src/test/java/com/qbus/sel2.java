204abdd7c72e62251f3c57c20449f5fb0dffc07b
Q-bas <kubyk.a@gmail.com>
2022-05-16 14:15:07 -0500

    median1

--- src/test/java/com/qbus/sel2.java
+++ src/test/java/com/qbus/sel2.java
@@ -1,25 +1,37 @@
 package com.qbus;
 
 import java.time.Duration;
-
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
 
 public class sel2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "/Users/andrew/dev0/files/chromedriver");
         WebDriver driver = new ChromeDriver();
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-
+        //open page
         driver.get("https://rahulshettyacademy.com/locatorspractice");
-        System.out.println("11111");
+        //login
+        String name = "qbus";
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.findElement(By.id("inputUsername")).sendKeys(name);
+        driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.className("submit")).click();
+        Thread.sleep(2000);
+        System.out.println(driver.findElement(By.tagName("p")).getText());
+        Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
+        Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(), "Hello "+name+",");
         
-        System.out.println("3");
-        System.out.println("3");
-        System.out.println("3");
-        System.out.println("3");
-        System.out.println("3");
+        //logout
+        driver.findElement(By.xpath("//button[text()='Log Out']")).click();
+
+        
+        //close window
+        driver.quit();
 
         
     }
