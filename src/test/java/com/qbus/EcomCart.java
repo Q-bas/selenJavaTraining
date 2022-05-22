1051316055fb52641180218a84deb409d0cd5041
Q-bas <kubyk.a@gmail.com>
2022-05-22 11:03:06 -0500

    explicit waits added

--- src/test/java/com/qbus/EcomCart.java
+++ src/test/java/com/qbus/EcomCart.java
@@ -1,5 +1,8 @@
 package com.qbus;
 
+import static org.junit.Assert.assertEquals;
+
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,16 +10,40 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 public class EcomCart {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "/Users/andrew/dev0/files/chromedriver");
         WebDriver driver = new ChromeDriver();
-        
+        //explicit wait declaration
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        //implicit wait - for each step in the class (commented as explicit wait used now)
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://rahulshettyacademy.com/seleniumPractise");
         //set array of products to add in cart
         String[] grocList ={"Cucumber","Brocolli","Beetroot"};
+        //add items to cart
+        addItems(driver,grocList);
+        //open cart popup
+        driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+        driver.findElement(By.xpath("//div[@class='cart-preview active']/div/button")).click();
+            //explicit wait usage
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input.promoCode")));
+        //apply promo code
+        driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.cssSelector(".promoBtn")).click();
+            //explicit wait use
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='promoInfo']")));
+        //check if allert shown
+        Assert.assertEquals(driver.findElement(By.xpath("//span[@class='promoInfo']")).getText(), "Code applied ..!"); 
 
-        driver.get("https://rahulshettyacademy.com/seleniumPractise");
+    }
+
+    public static void addItems(WebDriver driver,String[] grocList){
         //take list of all product names on page
         List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
         //iterator to break loop in the end
@@ -26,8 +53,7 @@ public class EcomCart {
             //get name + remove any extra chars + trim  from space
             String[] name = products.get(i).getText().split("-");
             String formatedName = name[0].trim();
-
-            //1. convet arrau to aray  list
+            //convert array to list
             List<String> li1 = Arrays.asList(grocList);
             //check if name conatins product from list grocList
             if(li1.contains(formatedName)){
@@ -35,8 +61,7 @@ public class EcomCart {
                 count1++;
                 driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
                 if(count1==grocList.length){
-                    break;
-                    
+                    break;                    
                 }
             }
             
