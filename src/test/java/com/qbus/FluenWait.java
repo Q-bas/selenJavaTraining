3d16fc303c59962c28f37bc597365ac6adebfa58
Q-bas <kubyk.a@gmail.com>
2022-05-24 17:18:34 -0500

    stupid warnning in fluent wait commented

--- src/test/java/com/qbus/FluenWait.java
+++ src/test/java/com/qbus/FluenWait.java
@@ -1,36 +1,37 @@
-package com.qbus;
+// package com.qbus;
 
-import java.time.Duration;
-import java.util.function.Function;
+// import java.time.Duration;
+// //import java.util.function.Function;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
+// import org.openqa.selenium.By;
+// import org.openqa.selenium.NoSuchElementException;
+// import org.openqa.selenium.WebDriver;
+// //import org.openqa.selenium.WebElement;
+// import org.openqa.selenium.chrome.ChromeDriver;
+// import org.openqa.selenium.support.ui.FluentWait;
+// import org.openqa.selenium.support.ui.Wait;
 
-public class FluenWait {
-    public static void main(String[] args) {
+// public class FluenWait {
+//     public static void main(String[] args) {
         
-        System.setProperty("webdriver.chrome.driver", "/Users/andrew/dev0/files/chromedriver");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
-        driver.findElement(By.cssSelector("[id='start'] button")).click();
+//         System.setProperty("webdriver.chrome.driver", "/Users/andrew/dev0/files/chromedriver");
+//         WebDriver driver = new ChromeDriver();
+//         driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+//         driver.findElement(By.cssSelector("[id='start'] button")).click();
         
-        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(30))
-            .pollingEvery(Duration.ofSeconds(3))
-            .ignoring(NoSuchElementException.class);
-        WebElement foo = wait.until(new Function<WebDriver, WebElement>(){
-            public WebElement apply(WebDriver driver){
-               if(driver.findElement(By.cssSelector("div[id='finish'] h4")).isDisplayed()){
-                   return driver.findElement(By.cssSelector("div[id='finish'] h4"));
-               }
-               else 
-                    return null;
-            }
-        });
-        System.out.println(driver.findElement(By.cssSelector("[id='finish'] h4")).getText());
-    }
-}
+//         // Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(30))
+//         //     .pollingEvery(Duration.ofSeconds(3))
+//         //     .ignoring(NoSuchElementException.class);
+//         //commented because of stupid warning
+//         // WebElement foo = wait.until(new Function<WebDriver, WebElement>(){
+//         //     public WebElement apply(WebDriver driver){
+//         //        if(driver.findElement(By.cssSelector("div[id='finish'] h4")).isDisplayed()){
+//         //            return driver.findElement(By.cssSelector("div[id='finish'] h4"));
+//         //        }
+//         //        else 
+//         //             return null;
+//         //     }
+//         // });
+//         System.out.println(driver.findElement(By.cssSelector("[id='finish'] h4")).getText());
+//     }
+// }
